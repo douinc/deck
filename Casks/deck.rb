05fb7ff209a5adec17d1d14393e2249f781f92d4
donny-son <d@dou.so>
2026-06-23 03:21:19 +0900

    Update Deck cask to v1.12

--- Casks/deck.rb
+++ Casks/deck.rb
@@ -1,6 +1,6 @@
 cask("deck") do
   version("1.12")
-  sha256("0000000000000000000000000000000000000000000000000000000000000000")
+  sha256("604d05e4f88e91b90fc60e615f99b9da346f5da6879b3f38ebc2f2a02955fe6e")
 
   url("https://github.com/douinc/deck/releases/download/v#{version}/Deck-#{version}.dmg")
   name("Deck")
